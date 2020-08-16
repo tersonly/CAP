4afac4de2fd4343693c899e2b2ab9a46af4a91ee
tersonly <20390658+tersonly@users.noreply.github.com>
2020-08-16 15:50:16 +0800

    增加注释

--- src/DotNetCore.CAP/CAP.ServiceCollectionExtensions.cs
+++ src/DotNetCore.CAP/CAP.ServiceCollectionExtensions.cs
@@ -36,32 +36,42 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.TryAddSingleton<CapMarkerService>();
 
-            //Consumer service
+            // 注入消费服务 实现了接口 ICapSubscribe    为下面的defaultSubscriberExecutor选择合适的excutor提供元数据
             AddSubscribeServices(services);
 
             //Serializer and model binder
+            //序列化 消息体
             services.TryAddSingleton<IContentSerializer, JsonContentSerializer>();
             services.TryAddSingleton<IMessagePacker, DefaultMessagePacker>();
+
+            //选择合适的消费者
             services.TryAddSingleton<IConsumerServiceSelector, DefaultConsumerServiceSelector>();
             services.TryAddSingleton<IModelBinderFactory, ModelBinderFactory>();
-
-            services.TryAddSingleton<ICallbackMessageSender, CallbackMessageSender>();
             services.TryAddSingleton<IConsumerInvokerFactory, ConsumerInvokerFactory>();
+
+            //发送消息后的回调
+            services.TryAddSingleton<ICallbackMessageSender, CallbackMessageSender>();
+
+            //缓存消费者执行的方法
             services.TryAddSingleton<MethodMatcherCache>();
 
-            //Processors
+            //处理器  hostService启动后 初始化 MQ的监听，Cap自己的逻辑 （处理需要重试的消息和需要删除过期的消息）  
             services.TryAddEnumerable(ServiceDescriptor.Singleton<IProcessingServer, ConsumerHandler>());
             services.TryAddEnumerable(ServiceDescriptor.Singleton<IProcessingServer, CapProcessingServer>());
             services.TryAddSingleton<IStateChanger, StateChanger>();
 
+            //队列的消息处理器
             //Queue's message processor
             services.TryAddSingleton<NeedRetryMessageProcessor>();
 
-            //Sender and Executors   
+            //单列  发送的消息和接受的消息的处理的缓冲区 先放到dispater中 最后统一处理 多线程处理        Sender and Executors   
             services.TryAddSingleton<IDispatcher, Dispatcher>();
+
+            //默认的消息处理器
             // Warning: IPublishMessageSender need to inject at extension project. 
             services.TryAddSingleton<ISubscriberExecutor, DefaultSubscriberExecutor>();
 
+            //初始化mq 和db的配置
             //Options and extension service
             var options = new CapOptions();
             setupAction(options);
@@ -69,10 +79,15 @@ namespace Microsoft.Extensions.DependencyInjection
             {
                 serviceExtension.AddServices(services);
             }
-            services.AddSingleton(options);
 
+            //注入配置  给依赖的服务调用
+            services.AddSingleton(options);
+            
+            //配置hostServer服务
             //Startup and Middleware
             services.AddTransient<IHostedService, DefaultBootstrapper>();
+
+            //启动consul的dashboard 后启动业务的中注册的中间件 比如mvc中间件
             services.AddTransient<IStartupFilter, CapStartupFilter>();
 
             return new CapBuilder(services);
